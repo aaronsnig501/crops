f21f059c87d96eb2936577d8861af7cfe8ccabdb
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-08-05 15:37:18 +0100

    Revert tab navigation to single page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import type { PageData } from "./$types"
 
-	import { page } from "$app/stores";
 	import PageStructure from "$lib/client/components/page/PageStructure.svelte";
-	import { TabGroup, TabAnchor, TreeView, TreeViewItem } from "@skeletonlabs/skeleton";
+	import { TabGroup, Tab, TreeView, TreeViewItem } from "@skeletonlabs/skeleton";
 
 	export let data: PageData
 
 	$: ({ categories } = data)
+
+	let tabSet: number = 0
 </script>
 
 <PageStructure>
@@ -38,18 +39,21 @@
 	</div>
 	<div slot="content">
 		<TabGroup justify="justify-center">
-			<TabAnchor href="/" selected={$page.url.pathname === "/"}>
+			<Tab bind:group={tabSet} name="tab1" value={0}>
 				Description
-			</TabAnchor>
-			<TabAnchor href="/growing" selected={$page.url.pathname === "/growing"}>
+			</Tab>
+			<Tab bind:group={tabSet} name="tab2" value={1}>
 				Growing
-			</TabAnchor>
-			<TabAnchor>
+			</Tab>
+			<Tab bind:group={tabSet} name="tab3" value={2}>
+				Storing
+			</Tab>
+			<Tab bind:group={tabSet} name="tab4" value={3}>
 				Cooking
-			</TabAnchor>
-			<TabAnchor>
-				Perserving
-			</TabAnchor>
+			</Tab>
+			<Tab bind:group={tabSet} name="tab5" value={4}>
+				Preserving
+			</Tab>
 			<svelte:fragment slot="panel">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt maiores vero neque in nisi debitis ratione illo officiis itaque, voluptatum voluptatibus aperiam error ut animi quas excepturi laudantium quasi aliquam.
 			</svelte:fragment>
