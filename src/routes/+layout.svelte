aa4a1a0c80cd1b5c602b2588d1a27d840afd3e3d
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-08-12 21:47:30 +0100

    Remove hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,16 +10,6 @@
 	</svelte:fragment>
 </AppBar>
 
-<main class="col-span-6">
-	<div class="bg-gradient-to-r from-green-400 via-green-300 to-green-200 w-screen">
-		<header class="py-56">
-			<h1 class="text-center text-5xl block">Crops
-				<small class="text-center text-xl block pt-4">by Smart Garden</small>
-			</h1>
-		</header>
-	</div>
-</main>
-
 <div class="container grid grid-cols-6 w-screen">
 	<slot />
 </div>
